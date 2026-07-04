f3e9738edb6edced58f66a765f9c17c2acd7f8f4
kapil rajput <gemlay.dev4@gemlay.com>
2026-07-04 17:59:44 +0530

    auto commit

--- src/shared/helpers/output/index.ts
+++ src/shared/helpers/output/index.ts
@@ -85,7 +85,7 @@ class Output {
     // console.log(chalk.red(`${title} ✖ ERROR`));
   }
 
-  async info(message: string, delay: number = 1) {
+  async info(message: string, delay: number = 3) {
     await writer.writeln(message, delay);
     // console.log(chalk.cyan(message));
   }
