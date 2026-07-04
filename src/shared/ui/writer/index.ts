b6661602efbeb4f92c9e1e012d37989828835630
kapil rajput <gemlay.dev4@gemlay.com>
2026-07-04 18:54:33 +0530

    auto commit

--- src/shared/ui/writer/index.ts
+++ src/shared/ui/writer/index.ts
@@ -7,7 +7,7 @@ export class Writer {
     }
   }
 
-  async writeln(text: string, delay = 18): Promise<void> {
+  async writeln(text: string, delay = 30): Promise<void> {
     await this.write(text, delay);
 
     process.stdout.write("\n");
