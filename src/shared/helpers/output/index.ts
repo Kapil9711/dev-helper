0695e1dc23b2fbab2300bb889df080e56c529180
kapil rajput <gemlay.dev4@gemlay.com>
2026-07-04 18:02:27 +0530

    auto commit

--- src/shared/helpers/output/index.ts
+++ src/shared/helpers/output/index.ts
@@ -115,7 +115,7 @@ class Output {
     // Ahead     : ${branch.ahead}
     // Behind    : ${branch.behind}
     // `;
-
+    await writer.writeln(chalk.cyan("Current Branch Details"));
     await output.info(chalk.cyan(table.toString()));
   }
 }
